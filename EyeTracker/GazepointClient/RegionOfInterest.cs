1c4c716ebd2aadb890daede55fb5d1a1da5f5c85
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-10-22 18:14:14 +0200

    Add ROI class

---
+++ EyeTracker/GazepointClient/RegionOfInterest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GazepointClient
+{
+
+    public class Point
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public class RegionOfInterest
+    {
+        public List<Point> points { get; set; }
+        public string region { get; set; }
+    }
+}
