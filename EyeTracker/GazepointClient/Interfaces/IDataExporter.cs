af18bfe86a48ddd4ade271bebcb2914e1a1a01e6
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-10-30 14:42:17 +0100

    Add data exporter interface

---
+++ EyeTracker/GazepointClient/Interfaces/IDataExporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GazepointClient.Interfaces
+{
+    public interface IDataExporter
+    {
+        void ExportData(Dictionary<string, List<object>> SignalObjectsDict, string savePath);
+    }
+}
