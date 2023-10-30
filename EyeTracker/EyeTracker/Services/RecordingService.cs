391b7b8d9713eee884bb2ce299c041fe9743dab4
Miloš Zeljko <miloszeljko00@gmail.com>
2023-10-30 09:00:34 +0100

    Screen recording working.

---
+++ EyeTracker/EyeTracker/Services/RecordingService.cs
@@ -0,0 +1,32 @@
+﻿using EyeTracker.Models;
+using OpenCvSharp;
+using ScreenRecorder;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EyeTracker.Services;
+
+public class RecordingService
+{
+    private Recorder? _recorder;
+    public RecordingService()
+    {
+    }
+
+    public void StartRecordingScreen()
+    {
+        if (_recorder != null) return;
+        _recorder = new Recorder(new RecorderParams("REC.avi", 24, SharpAvi.KnownFourCCs.Codecs.MotionJpeg, 100));
+    }
+    public void StopRecordingScreen()
+    {
+        if(_recorder == null) return;
+        _recorder.Dispose();
+        _recorder = null;
+    }
+}
